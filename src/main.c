2805303263e926e86c1faf45fd1c03698aedcfaf
Oliver Wiedemann <mail@oliver-wiedemann.net>
2019-12-03 16:54:38 +0100

    modify decoder_init to source_decoder_init

--- src/main.c
+++ src/main.c
@@ -282,18 +282,42 @@ void context_free(reader_context **r_ctx, decoder_context **d_ctx)
  * @param queue_capacity output frame queue capacity.
  * @return decoder_context* with all members initialized.
  */
-decoder_context *decoder_init(reader_context *r_ctx, int queue_capacity)
+decoder_context *source_decoder_init(reader_context *r_ctx, int queue_capacity)
 {
+	AVCodecContext *avctx;
 	decoder_context *d;
+	int ret;
+	int index = r_ctx->stream_index;
+	AVStream *stream = r_ctx->format_ctx->streams[index];
+	AVCodec *codec;
+
+	avctx = avcodec_alloc_context3(NULL);
+	if (!avctx)
+		pexit("avcodec_alloc_context3 failed");
+
+	ret = avcodec_parameters_to_context(avctx, stream->codecpar);
+	if (ret < 0)
+		pexit("avcodec_parameters_to_context failed");
+
+	avctx->time_base = stream->time_base;
+
+	codec = avcodec_find_decoder(avctx->codec_id);
+	if (!codec)
+		pexit("avcodec_find_decoder failed");
+
+	avctx->codec_id = codec->id;
+
+	ret = avcodec_open2(avctx, codec, NULL);
+	if (ret < 0)
+		pexit("avcodec_open2 failed");
 
 	d = malloc(sizeof(decoder_context));
 	if (!d)
 		pexit("malloc failed");
 
-	d->format_ctx = r_ctx->format_ctx;
 	d->packet_queue = r_ctx->packet_queue;
-	d->stream_index = r_ctx->stream_index;
 	d->frame_queue = create_queue(queue_capacity);
+	d->avctx = avctx;
 
 	return d;
 }
